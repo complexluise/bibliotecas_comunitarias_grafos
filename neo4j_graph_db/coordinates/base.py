19331012782bbd76240a5947ce40ba39ba35d3ec
complexluise <lhiguerac@unal.edu.co>
2024-11-15 22:13:41 -0500

    📦✨ feat: add base AnalysisCoordinate abstract class
    Add an abstract class 'AnalysisCoordinate' in 'base.py' under 'neo4j_graph_db/coordinates'. This abstract class provides a blueprint for future classes dealing with coordinates analysis. It includes abstract methods 'get_data' and 'calculate_score' which must be implemented in any child classes.

---
+++ neo4j_graph_db/coordinates/base.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from pandas import DataFrame
+
+
+class AnalysisCoordinate(ABC):
+    def __init__(self, driver=None, df_encuestas=None, name: str = '', description: str = ''):
+        self.driver = driver
+        self.df_encuestas = df_encuestas
+        self.category = None
+        self.name = name
+        self.description = description
+        self.score = None
+
+    @abstractmethod
+    def get_data(self) -> DataFrame:
+        raise NotImplementedError
+
+    @abstractmethod
+    def calculate_score(self, bibliotecas: list[str]) -> DataFrame:
+        raise NotImplementedError
