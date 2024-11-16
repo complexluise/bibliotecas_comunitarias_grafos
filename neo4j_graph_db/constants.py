27ea4e5c5ea12536c52c4aa7c8d8ba8bd6502c5c
complexluise <lhiguerac@unal.edu.co>
2024-11-15 22:14:06 -0500

    📝✨feat(constants.py): Add AnalysisCategory Enum

---
+++ neo4j_graph_db/constants.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class AnalysisCategory(Enum):
+    CATALOGO_DIGITALIZACION = "Nivel de avance en la digitalización del catálogo"
+    SISTEMATIZACION_SERVICIOS = "Nivel de avance en la sistematización de servicios bibliotecarios"
+    COLECCION_CARACTERIZACION = "Caracterización de la colección"
+    FACILIDAD_ADOPCION_KOHA = "Facilidad de Adopción del Catálogo en Koha"
+    IMPACTO_ADOPTAR_KOHA = "Impacto de Adoptar Koha"
+    DETALLE_COLECCION_KOHA = "Detalle Colección Koha"
