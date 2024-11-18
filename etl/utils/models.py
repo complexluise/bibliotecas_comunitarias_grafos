af3510ec4c688c1fa7db0bdb0d794f6b71511613
complexluise <lhiguerac@unal.edu.co>
2024-11-18 03:23:56 -0500

    📝✨feat(models.py): Add Neo4JConfig dataclass for database connection configuration

---
+++ etl/utils/models.py
@@ -0,0 +1,12 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Neo4JConfig:
+    """
+    Configuration class for Neo4J database connection.
+    """
+    uri: str = os.getenv("NEO4J_URI")
+    user: str = os.getenv("NEO4J_URI")
+    password: str = os.getenv("NEO4J_URI")
