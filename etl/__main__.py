fcf8d676b22c5abc4f70a445af45231043c68d42
complexluise <lhiguerac@unal.edu.co>
2024-11-19 00:29:05 -0500

    🔧 chore(etl/__main__.py): agrega el directorio del proyecto al path de Python para mejorar la importación de módulos

--- etl/__main__.py
+++ etl/__main__.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the project root to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 from etl.cli import cli
 
 if __name__ == "__main__":
