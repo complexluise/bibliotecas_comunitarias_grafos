d7e60e3859e4c9d1da02d1c4e50882df0fb38422
complexluise <lhiguerac@unal.edu.co>
2024-11-19 18:02:58 -0500

    🛠️ refactor(utils/query_manager): elimina métodos obsoletos y actualiza consultas para mejorar la gestión de datos.

--- etl/utils/query_manager.py
+++ etl/utils/query_manager.py
@@ -1,131 +1,6 @@
 class Neo4JQueryManager:
 
     @staticmethod
-    def create_biblioteca_comunitaria() -> str:
-        return """
-        CREATE (b:BibliotecaComunitaria)
-        SET b += $props_biblioteca
-        """
-
-    @staticmethod
-    def create_and_link_ubicacion() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        CREATE (u:Ubicacion)
-        SET u += $props_ubicacion
-        CREATE (b)-[:UBICADA_EN]->(u)
-        """
-
-    @staticmethod
-    def create_and_link_localidad() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (l:Localidad {nombre: $nombre_localidad})
-        CREATE (b)-[:PERTENECE_A]->(l)
-        """
-
-    @staticmethod
-    def create_and_link_redes_sociales() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        CREATE (rs:RedesSociales)
-        SET rs += $props_redes_sociales
-        CREATE (b)-[:TIENE_REDES_SOCIALES]->(rs)
-        """
-
-    @staticmethod
-    def create_and_link_coleccion() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        CREATE (c:Coleccion)
-        SET c += $props_coleccion
-        CREATE (b)-[:TIENE_COLECCION]->(c)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_coleccion() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (tc:TipoColeccion {nombre: $nombre_tipo})
-        CREATE (b)-[:TIENE_TIPO_COLECCION]->(tc)
-        """
-
-    @staticmethod
-    def create_and_link_catalogo() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        CREATE (cat:Catalogo)
-        SET cat += $props_catalogo
-        CREATE (b)-[:TIENE_CATALOGO]->(cat)
-        """
-
-    @staticmethod
-    def create_and_link_soporte_catalogo() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        CREATE (sc:SoporteCatalogo)
-        SET sc += $props_soporte_catalogo
-        CREATE (b)-[:USA_SOPORTE_CATALOGO]->(sc)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_servicio() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (ts:TipoServicio {nombre: $nombre_tipo})
-        CREATE (b)-[:OFRECE]->(ts)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_actividad() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (ta:TipoActividad {nombre: $nombre_tipo})
-        CREATE (b)-[:REALIZA]->(ta)
-        """
-
-    @staticmethod
-    def create_and_link_tecnologia() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        CREATE (t:Tecnologia)
-        SET t += $props_tecnologia
-        CREATE (b)-[:USA_TECNOLOGIA]->(t)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_tecnologia() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (tt:TipoTecnologia {nombre: $nombre_tipo})
-        CREATE (b)-[:TIENE_TECNOLOGIA]->(tt)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_poblacion() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (tp:TipoPoblacion {nombre: $nombre_tipo})
-        CREATE (b)-[:ATIENDE]->(tp)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_aliado() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (ta:TipoAliado {nombre: $nombre_tipo})
-        CREATE (b)-[:ALIADA_CON]->(ta)
-        """
-
-    @staticmethod
-    def create_and_link_tipo_financiacion() -> str:
-        return """
-        MATCH (b:BibliotecaComunitaria {id: $id_biblioteca})
-        MERGE (tf:TipoFinanciacion {nombre: $nombre_tipo})
-        CREATE (b)-[:FINANCIADA_POR]->(tf)
-        """
-
-    @staticmethod
     def infraestructura_tecnologica():
         return """MATCH (b:BibliotecaComunitaria)
         OPTIONAL MATCH (b)-[r1:TIENE_TECNOLOGIA]->(t1:Tipos_Tecnologia)
@@ -140,12 +15,12 @@ class Neo4JQueryManager:
     @staticmethod
     def diversidad_colecciones():
         return """
-        MATCH (b:BibliotecaComunitaria)-[:TIENE_COLECCION]->(c:Coleccion)
-        RETURN b.id AS BibliotecaID, collect(DISTINCT c.tipo) AS tipos_coleccion
+        MATCH (b:BibliotecaComunitaria)-[:CONTIENE_TIPO]->(c:Tipos_Coleccion)
+        RETURN b.id AS BibliotecaID, collect(DISTINCT c.nombre) AS tipos_coleccion
         """
 
     @staticmethod
-    def cantidad_material_bibliografico():
+    def cantidad_inventario():
         return """
         MATCH (b:BibliotecaComunitaria)
         RETURN b.id AS BibliotecaID, b.cantidad_inventario AS cantidad_inventario
