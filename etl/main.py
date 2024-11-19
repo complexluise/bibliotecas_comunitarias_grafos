3f7060b4609531e0ed31245b55017ac873aa2e4a
complexluise <lhiguerac@unal.edu.co>
2024-11-19 00:12:37 -0500

    ✨ refactor(etl/main.py): reorganiza funciones para procesar el grafo de conocimiento y la operacionalización de datos.

--- etl/main.py
+++ etl/main.py
@@ -8,21 +8,30 @@ from etl.transformers.bibliotecas_transformer import BibliotecasTransformer
 
 registrador = setup_logger("etl_main.log")
 
-
-def main():
-    config = Neo4JConfig()
-
-    bibliotecas_pipeline = ETLPipeline(
+def process_knowledge_graph(config: Neo4JConfig):
+    pipeline = ETLPipeline(
         source=CSVDataSource(
             "data/BASE DE DATOS DE BIBLIOTECAS COMUNITARIAS DE BOGOTÁ - SIBIBO 2024 - Base de datos.csv"
         ),
         transformer=BibliotecasTransformer(),
         destination=Neo4jDestination(config),
     )
+    pipeline.add_observer(ETLObserver())
+    pipeline.execute()
 
-    bibliotecas_pipeline.add_observer(ETLObserver())
-    bibliotecas_pipeline.execute()
+def process_operationalization(config: Neo4JConfig):
+    pipeline = ETLPipeline(
+        source=OperationalizationSource(config, "data/Contacto Bibliotecas - Formulario Coordenadas.csv"),
+        transformer=OperationalizationTransformer(),
+        destination=CSVDestination("output/analysis_results.csv")
+    )
+    pipeline.add_observer(ETLObserver())
+    pipeline.execute()
 
+def main():
+    config = Neo4JConfig()
+    process_knowledge_graph(config)
+    process_operationalization(config)
 
 if __name__ == "__main__":
     main()
