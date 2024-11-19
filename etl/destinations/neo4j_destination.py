36ac3879085476a28fa059e33764c30df057fdd5
complexluise <lhiguerac@unal.edu.co>
2024-11-18 23:35:52 -0500

    ✨ feat(neo4j_destination.py): añade clase Neo4jDestination para cargar datos en Neo4J desde la configuración.

---
+++ etl/destinations/neo4j_destination.py
@@ -0,0 +1,16 @@
+from etl.core.base import DataDestination
+from etl.utils.models import Neo4JConfig
+from etl.neo4j_import_db_SiBiBo import cargar_datos_en_neo4j
+
+
+class Neo4jDestination(DataDestination):
+    def __init__(self, config: Neo4JConfig):
+        self.config = config
+
+    def load(self, data: list[dict]):
+        cargar_datos_en_neo4j(
+            uri=self.config.uri,
+            usuario=self.config.user,
+            contraseña=self.config.password,
+            datos=data
+        )
