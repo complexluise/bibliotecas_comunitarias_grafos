a6a997cb97d0b722de1cd94a65a7db075acee65c
complexluise <lhiguerac@unal.edu.co>
2024-11-19 00:44:27 -0500

    🔧 fix(etl/destinations/csv_destination): mejora la creación de directorios para la salida de CSV.

--- etl/destinations/csv_destination.py
+++ etl/destinations/csv_destination.py
@@ -1,13 +1,15 @@
 from etl.core.base import DataDestination
 import pandas as pd
+from pathlib import Path
 
 
 class CSVDestination(DataDestination):
     def __init__(self, output_path: str):
-        self.output_path = output_path
+        self.output_path = Path(output_path)
 
     def load(self, data: pd.DataFrame):
         self._write(data)
 
     def _write(self, data: pd.DataFrame):
+        self.output_path.parent.mkdir(parents=True, exist_ok=True)
         data.to_csv(self.output_path, index=False)
