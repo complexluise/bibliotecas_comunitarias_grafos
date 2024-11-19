c810a97b18bfb84e7bd339e1df4d24da89a5d6c3
complexluise <lhiguerac@unal.edu.co>
2024-11-18 23:34:49 -0500

    ✨ feat(etl/core/base.py): agrega clases abstractas para la extracción, transformación y carga de datos

---
+++ etl/core/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+class DataSource(ABC):
+    @abstractmethod
+    def extract(self) -> List[Dict]:
+        pass
+
+class DataDestination(ABC):
+    @abstractmethod
+    def load(self, data: List[Dict]):
+        pass
+
+class DataTransformer(ABC):
+    @abstractmethod
+    def transform(self, data: List[Dict]) -> List[Dict]:
+        pass
